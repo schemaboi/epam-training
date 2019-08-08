69af32f49206bc56411e213f7ffe37ef19b84734
Roman Moravskyi <moravsky12@gmail.com>
2019-08-08 12:58:39 +0300

    added StaticCircle class

---
+++ ClassTask/StaticCircle.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace Epam.HomeWork.ClassTask
+{
+    public static class StaticCircle
+    {
+        public const double PI = 3.14159265359;
+
+        public static double CalculateArea(double radius)
+        {
+            if (radius < 0)
+                throw new ArgumentException("Negative radius!");
+
+            return PI * radius * radius;
+        }
+
+        public static double CalculateLength(double radius)
+        {
+            if (radius < 0)
+                throw new ArgumentException("Negative radius!");
+
+            return 2 * PI * radius;
+        }
+    }
+}
