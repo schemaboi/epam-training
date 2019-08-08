ef7e405f64f3baa4aa96e86a527ed8de3500c0e6
Roman Moravskyi <moravsky12@gmail.com>
2019-08-08 12:59:16 +0300

    added functionality for Circle class

--- ClassTask/Circle.cs
+++ ClassTask/Circle.cs
@@ -6,5 +6,31 @@ namespace Epam.HomeWork.ClassTask
 {
     public class Circle
     {
+        public const double PI = 3.14159265359;
+        private double _radius;
+
+        public Circle(double radius = 0.0)
+        {
+            Radius = radius;
+        }
+
+        public double Radius
+        {
+            get
+            {
+                return _radius;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Negative radius!");
+
+                _radius = value;
+            }
+        }
+
+        public double Area { get => PI * Radius * Radius; }
+        public double Length { get => 2 * PI * Radius; }
+
     }
 }
