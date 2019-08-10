4ddaaae2b03dd822a19f311f0b1a656efee6fd18
Roman Moravskyi <moravsky12@gmail.com>
2019-08-10 14:14:07 +0300

    YAGNI (operator+)

--- ClassTask/Point.cs
+++ ClassTask/Point.cs
@@ -14,10 +14,5 @@ namespace Epam.HomeWork.ClassTask
 
         public double X { get; set; }
         public double Y { get; set; }
-
-        public static Point operator+(Point left, Point right)
-        {
-            return new Point(left.X + right.X, left.Y + right.Y);
-        }
     }
 }
