884545db15a5ab516d2153df907998c08c4c1cfa
Roman Moravskyi <moravsky12@gmail.com>
2019-08-17 00:07:36 +0300

    created class for second training task runner

---
+++ TaskRunner/SecondTrainingTaskRunner.cs
@@ -0,0 +1,24 @@
+﻿namespace Epam.HomeWork
+{
+    using System;
+    using Training_1;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class SecondTrainingTaskRunner : TaskRunner, IRunable
+    {
+        public void Run()
+        {
+        }
+
+        private void RunTask1()
+        {
+           
+        }
+
+        private void RunTask3()
+        {
+            
+        }
+    }
+}
