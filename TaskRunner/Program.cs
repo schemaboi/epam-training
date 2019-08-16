f3c8ae6a17084f65232be1f0b6c3437b78e89715
Roman Moravskyi <moravsky12@gmail.com>
2019-08-17 00:06:20 +0300

    added Program.cs

---
+++ TaskRunner/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Epam.HomeWork
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class Program
+    {
+        static void Main(string[] args) 
+        {
+            var tasks = new List<IRunable>
+            {
+                new FirstTrainingTaskRunner(),
+                new SecondTrainingTaskRunner()
+            };
+
+            foreach(var task in tasks)
+            {
+                task.Run();
+            }
+
+            TaskRunner.ConsolePause();
+        }
+    }
+}
