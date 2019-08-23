0464cea3643d3d28fa2d718b54a62c66f2cc8e56
Roman Moravskyi <moravsky12@gmail.com>
2019-08-23 13:32:16 +0300

    implemented ContactNumber struct

---
+++ Training_3/ContactNumber.cs
@@ -0,0 +1,20 @@
+﻿namespace Epam.HomeWork.Training_3
+{
+    public struct ContactNumber
+    {
+        public ContactNumber(string name = "", string phoneNumber = "")
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Number: {PhoneNumber}";
+        }
+    }
+}
